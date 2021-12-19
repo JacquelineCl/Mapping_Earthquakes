7b580a4158dc116489f48f45d48cddedeccb1a25
JaxC <90055896+JacquelineCL@users.noreply.github.com>
2021-12-18 20:24:47 -0500

    through 13.5.3

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level standard view
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 //  Add a marker to the map for Los Angeles, California.
 // L.circleMarker([34.0522, -118.2437], {
@@ -30,7 +30,7 @@ cityData.forEach(function(city) {
 // We create the tile layer that will be the background of our map.
 var outdoorMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 4,
+    maxZoom: 14,
     id: 'mapbox/outdoors-v11',
     accessToken: API_KEY
 });
@@ -48,5 +48,42 @@ L.polyline(line, {
     color: "yellow"
   }).addTo(map);
 
+  // Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
 // Then we add our 'graymap' tile layer to the map.
 outdoorMap.addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.name + "</h2>" + "<br>" + feature.properties.city + ", " + feature.properties.country );
+    }
+  
+  //onEachFeature not showing information in popup  
+  // onEachFeature: function(feature, layer) {
+  //   console.log(layer);
+  //   layer.bindPopup();
+  // }
+
+}).addTo(map);
