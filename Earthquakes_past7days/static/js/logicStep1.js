8fe86ca86eaf11947e0a0fd12ca7524c0268d5cd
JaxC <90055896+JacquelineCL@users.noreply.github.com>
2021-12-18 22:06:27 -0500

    earthquake data added

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,9 +18,14 @@ let baseMaps = {
 };
 
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 })
 
 L.control.layers(baseMaps).addTo(map);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
